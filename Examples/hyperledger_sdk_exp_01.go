c779d01878c72e721ce2c4cf6be0343aaa399fe1
A-Hashemian <90988199+A-Hashemian@users.noreply.github.com>
2023-03-07 12:24:24 -0500

    up_01

--- Examples/hyperledger_sdk_exp_01.go
+++ Examples/hyperledger_sdk_exp_01.go
@@ -1,1 +1,22 @@
 
+package main
+
+import (
+    "fmt"
+    "github.com/hyperledger/fabric-sdk-go/pkg/client/channel"
+    "github.com/hyperledger/fabric-sdk-go/pkg/common/errors/retry"
+    "github.com/hyperledger/fabric-sdk-go/pkg/common/providers/msp"
+    "github.com/hyperledger/fabric-sdk-go/pkg/core/config"
+    "github.com/hyperledger/fabric-sdk-go/pkg/fabsdk"
+)
+
+func main() {
+    // Create SDK setup for the integration tests
+    sdk, err := fabsdk.New(config.FromFile("./connection.yaml"))
+    if err != nil {
+        fmt.Println("Failed to create new SDK: ", err)
+        return
+    }
+  
+  
+}
