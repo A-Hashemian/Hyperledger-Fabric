8ff896c092561807a82ef0f097abc7429f75a850
A-Hashemian <90988199+A-Hashemian@users.noreply.github.com>
2023-03-07 10:40:50 -0500

    up_01

--- Examples/User_01.go
+++ Examples/User_01.go
@@ -1,1 +1,10 @@
 
+package main
+
+import (
+    "fmt"
+    "github.com/hyperledger/fabric-sdk-go/pkg/client/channel"
+    "github.com/hyperledger/fabric-sdk-go/pkg/common/providers/msp"
+    "github.com/hyperledger/fabric-sdk-go/pkg/core/config"
+    "github.com/hyperledger/fabric-sdk-go/pkg/fabsdk"
+)
