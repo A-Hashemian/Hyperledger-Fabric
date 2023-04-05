74c7786135af55a6dbfbe3c076bf85b08883da5d
A-Hashemian <90988199+A-Hashemian@users.noreply.github.com>
2023-04-05 23:14:20 +0300

    Update contract_00.go

--- Chaincode/contract_00.go
+++ Chaincode/contract_00.go
@@ -6,3 +6,9 @@ import (
 
 	"github.com/hyperledger/fabric-contract-api-go/contractapi"
 )
+
+// SimpleAsset is a basic struct for the asset
+type SimpleAsset struct {
+	AssetID string `json:"assetID"`
+	Value   int    `json:"value"`
+}
