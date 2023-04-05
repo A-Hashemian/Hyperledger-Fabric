1983171b9335e116c83dfaae7fc5adbbfdf8bd4e
A-Hashemian <90988199+A-Hashemian@users.noreply.github.com>
2023-04-05 23:16:32 +0300

    Update contract_00.go

--- Chaincode/contract_00.go
+++ Chaincode/contract_00.go
@@ -18,3 +18,14 @@ type SimpleAsset struct {
 type SmartContract struct {
 	contractapi.Contract
 }
+
+
+// InitLedger adds a base set of assets to the ledger
+func (s *SmartContract) InitLedger(ctx contractapi.TransactionContextInterface) error {
+	assets := []SimpleAsset{
+		{AssetID: "asset1", Value: 100},
+		{AssetID: "asset2", Value: 200},
+		{AssetID: "asset3", Value: 300},
+		{AssetID: "asset4", Value: 400},
+		{AssetID: "asset5", Value: 500},
+	}
